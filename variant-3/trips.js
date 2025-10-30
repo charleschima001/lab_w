7ae8e16f582eab6bb59a3e0af4a74996bbdf119e
charleschima001 <chukwuemekachima001@gmail.com>
2025-10-30 14:21:07 +0300

    Delete variant-3 directory

--- variant-3/trips.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const model = new TripModel();
-    const view = new TripView();
-    const presenter = new TripPresenter(model, view);
-});
